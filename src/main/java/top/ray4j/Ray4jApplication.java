9c74b3559ec6c8f7cd55a290b1e80eb54bbb854a
chenrui <185048761@qq.com>
2022-07-07 16:41:14 +0800

    init

---
+++ src/main/java/top/ray4j/Ray4jApplication.java
@@ -0,0 +1,15 @@
+package top.ray4j;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author: Cr.
+ * @date: 2022/7/7
+ */
+public class Ray4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ray4jApplication.class, args);
+    }
+
+}
