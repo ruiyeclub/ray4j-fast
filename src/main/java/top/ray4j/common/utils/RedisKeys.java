fee9fd06d646fd9af17ae70ce913a00f0d7399e7
RuiyeClub <ruiyeclub@foxmail.com>
2022-07-08 20:25:48 +0800

    feat: utils

---
+++ src/main/java/top/ray4j/common/utils/RedisKeys.java
@@ -0,0 +1,13 @@
+package top.ray4j.common.utils;
+
+/**
+ * Redis所有Keys
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+public class RedisKeys {
+
+    public static String getSysConfigKey(String key) {
+        return "sys:config:" + key;
+    }
+}
