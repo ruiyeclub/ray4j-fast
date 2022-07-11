ec57475402635ca30f645ba42f78ac7f0a73d583
chenrui <185048761@qq.com>
2022-07-11 15:29:58 +0800

    feat: utils

--- src/main/java/top/ray4j/common/utils/Query.java
+++ src/main/java/top/ray4j/common/utils/Query.java
@@ -19,7 +19,7 @@ public class Query<T> {
     }
 
     public IPage<T> getPage(Map<String, Object> params, String defaultOrderField, boolean isAsc) {
-        //分页参数
+        // 分页参数
         long curPage = 1;
         long limit = 10;
 
@@ -30,19 +30,19 @@ public class Query<T> {
             limit = Long.parseLong((String) params.get(Constant.LIMIT));
         }
 
-        //分页对象
+        // 分页对象
         Page<T> page = new Page<>(curPage, limit);
 
-        //分页参数
+        // 分页参数
         params.put(Constant.PAGE, page);
 
-        //排序字段
-        //防止SQL注入（因为sidx、order是通过拼接SQL实现排序的，会有SQL注入风险）
+        // 排序字段
+        // 防止SQL注入（因为sidx、order是通过拼接SQL实现排序的，会有SQL注入风险）
         String orderField = SQLFilter.sqlInject((String) params.get(Constant.ORDER_FIELD));
         String order = (String) params.get(Constant.ORDER);
 
 
-        //前端字段排序
+        // 前端字段排序
         if (StringUtils.isNotEmpty(orderField) && StringUtils.isNotEmpty(order)) {
             if (Constant.ASC.equalsIgnoreCase(order)) {
                 return page.addOrder(OrderItem.asc(orderField));
@@ -51,12 +51,12 @@ public class Query<T> {
             }
         }
 
-        //没有排序字段，则不排序
+        // 没有排序字段，则不排序
         if (StringUtils.isBlank(defaultOrderField)) {
             return page;
         }
 
-        //默认排序
+        // 默认排序
         if (isAsc) {
             page.addOrder(OrderItem.asc(defaultOrderField));
         } else {
