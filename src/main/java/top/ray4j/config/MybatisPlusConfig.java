f06712056a63b4a594ab91ae103b6c38b8f7508d
chenrui <185048761@qq.com>
2022-07-14 17:52:51 +0800

    feat: add config

---
+++ src/main/java/top/ray4j/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package top.ray4j.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis-plus配置
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
